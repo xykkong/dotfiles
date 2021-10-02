74a731a5c1ce0c17c7bb1390b6a9d17366a2d8e9
Xiao <xyk.kong@gmail.com>
2021-10-02 20:51:17 -0300

    update git config

--- git/install.sh
+++ git/install.sh
@@ -5,11 +5,9 @@ dest=$HOME
 
 file1='.gitconfig'
 file2='.gitconfig_alias'
-file3='.gitattributes'
-file4='.gitignore_global'
+file3='.gitignore_global'
 
 
 ln -s $src/$file1 $dest/$file1
 ln -s $src/$file2 $dest/$file2
 ln -s $src/$file3 $dest/$file3
-ln -s $src/$file4 $dest/$file4
