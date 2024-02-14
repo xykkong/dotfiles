da104dea64e09ab4352ad7289ce1e598fb25d071
Xiao <xyk.kong@gmail.com>
2024-02-14 06:07:27 -0300

    fixing script

--- kitty/install.sh
+++ kitty/install.sh
@@ -3,9 +3,15 @@
 src=$(pwd)
 dest=$HOME/.config/kitty
 
-files=("cursor.conf" "font.conf" "kitty.conf" "macos.conf" "mouse.conf" "performance.conf" "session.conf" "settings.conf" "shell.conf" "ui" "tab_bar.py" "tab_bar.conf" "vi-mode.lua")
+files=("cursor.conf" "font.conf" "keybinds.conf" "kitty.conf" "macos.conf" "mouse.conf" "performance.conf" "session.conf" "settings.conf" "shell.conf" "tab_bar.py" "tab_bar.conf" "vi-mode.lua")
+uis=("cattpuccin_macchiato.conf" "sonokai.conf")
 
 mkdir -p $dest
 for file in ${files[@]}; do
-  ln -fs $src/$file $dest/$file
+	ln -fs $src/$file $dest/$file
+done
+
+mkdir -p $dest/ui
+for file in ${uis[@]}; do
+	ln -fs $src/ui/$file $dest/ui/$file
 done
